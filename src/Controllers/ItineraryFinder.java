ecc6c9d6eb9e10143908ce89954a54d3cc53928c
Adam Wolf <aw1875@rit.edu>
2019-10-27 17:04:11 -0400

    Completed Itinerary Finder functionality

--- src/Controllers/ItineraryFinder.java
+++ src/Controllers/ItineraryFinder.java
@@ -3,29 +3,43 @@ package Controllers;
 import Helpers.SceneBuilder;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 
 public class ItineraryFinder {
     private JPanel itineraryPanel;
     private JTabbedPane tabbedPane1;
     private JTextField localOriginTextfield;
     private JTextField localDestinationTextfield;
-    private JComboBox comboBox1;
-    private JSpinner spinner1;
+    private JComboBox localSortingMethod;
+    private JSpinner localConnections;
     private JButton localSubmitButton;
     private JTextField onlineOriginTextfield;
     private JTextField onlineDestinationTextfield;
     private JButton onlineSubmitButton;
-    private JComboBox comboBox2;
-    private JSpinner spinner2;
+    private JComboBox onlineSortingMethod;
+    private JSpinner onlineConnections;
     private JButton connectButton;
     private JButton disconnectButton;
 
     public ItineraryFinder() {
+
+        /**
+         * Set Connection Spinners up
+         */
+        localConnections.setModel(new SpinnerNumberModel(0, 0, 2, 1));
+        onlineConnections.setModel(new SpinnerNumberModel(0, 0, 2, 1));
+
+        /**
+         * Set Sorting Method Combobox's up
+         */
+        String [] sortMethods = {"Departure Time (Default)", "Arrival Time", "Airfare"};
+        for (int i = 0; i < sortMethods.length; i++) {
+            localSortingMethod.addItem(sortMethods[i]);
+            onlineSortingMethod.addItem(sortMethods[i]);
+        }
 
         /**
          * Hover event listeners
@@ -103,21 +117,104 @@ public class ItineraryFinder {
             public void actionPerformed(ActionEvent e) {
                 connectButton.setEnabled(false);
                 connectButton.setBackground(Color.decode("#E8E8E8"));
+                onlineSubmitButton.setEnabled(true);
+                onlineSubmitButton.setBackground(Color.decode("#3C97D9"));
                 disconnectButton.setEnabled(true);
                 disconnectButton.setBackground(Color.decode("#3C97D9"));
             }
         });
+
+        /**
+         * Disconnect Pressed Event
+         */
         disconnectButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 connectButton.setEnabled(true);
                 connectButton.setBackground(Color.decode("#3C97D9"));
+                onlineSubmitButton.setEnabled(false);
+                onlineSubmitButton.setBackground(Color.decode("#E8E8E8"));
                 disconnectButton.setEnabled(false);
                 disconnectButton.setBackground(Color.decode("#E8E8E8"));
             }
         });
+
+        /**
+         * Switched Tab Event
+         */
+        tabbedPane1.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent changeEvent) {
+                JTabbedPane tabbedPane = (JTabbedPane) changeEvent.getSource();
+                int selectedIndex = tabbedPane.getSelectedIndex();
+
+                if (selectedIndex == 0) {
+                    //TODO Add disconnect functionality & remove print
+                    System.out.println("Changed to Local Tab");
+                }
+            }
+        });
+
+        /**
+         * Submit button events
+         */
+        localSubmitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                if (localOriginTextfield.getText().equals("") || localDestinationTextfield.getText().equals("")) {
+                    JOptionPane.showMessageDialog(JOptionPane.getRootFrame(),
+                            "Please enter both Origin and Destination airport codes.",
+                            "Missing Inputs",
+                            JOptionPane.ERROR_MESSAGE);
+                } else {
+                    String sortMethod = "";
+                    switch (localSortingMethod.getSelectedIndex()) {
+                        case 0:
+                            sortMethod = "departure";
+                            break;
+                        case 1:
+                            sortMethod = "arrival";
+                            break;
+                        case 2:
+                            sortMethod = "airfare";
+                            break;
+                    }
+                    String command = "info," + localOriginTextfield.getText() + "," + localDestinationTextfield.getText() + "," + localConnections.getValue() + "," + sortMethod + ";";
+                    System.out.println(command);
+                }
+            }
+        });
+        onlineSubmitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                if (onlineOriginTextfield.getText().equals("") || onlineDestinationTextfield.getText().equals("")) {
+                    JOptionPane.showMessageDialog(JOptionPane.getRootFrame(),
+                            "Please enter both Origin and Destination airport codes.",
+                            "Missing Inputs",
+                            JOptionPane.ERROR_MESSAGE);
+                } else {
+                    String sortMethod = "";
+                    switch (onlineSortingMethod.getSelectedIndex()) {
+                        case 0:
+                            sortMethod = "departure";
+                            break;
+                        case 1:
+                            sortMethod = "arrival";
+                            break;
+                        case 2:
+                            sortMethod = "airfare";
+                            break;
+                    }
+                    String command = "info," + onlineOriginTextfield.getText() + "," + onlineDestinationTextfield.getText() + "," + onlineConnections.getValue() + "," + sortMethod + ";";
+                    System.out.println(command);
+                }
+            }
+        });
     }
 
+    /**
+     * Build the Scene using SceneBuilder helper class
+     */
     public static void buildScene() {
         SceneBuilder.buildScene("AFRS: Itinerary Finder", new ItineraryFinder().itineraryPanel, new Dimension(400, 288), JFrame.DISPOSE_ON_CLOSE);
     }
